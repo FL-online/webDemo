d61f7e884f1936caffba6675ab3f0e93f02e774f
Franziska Ledig <fl32zyvy@studserv.uni-leipzig.de>
2024-10-11 17:23:31 +0200

    initial commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+rechtsklick und öffnen mit auf index file zum anschauen
+
+Website zu testzwecken, HTML, CSS, Javascript
+noch in Arbeit, besonders reaktive Teile
---
+++ scripts/interaktiv.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", updateName);
+
+function updateName() {
+  window.location.href "history.html";
+}
