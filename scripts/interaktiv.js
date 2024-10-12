2f3b8b6ec533f96d0be3a3e6f74fb15c0851f71b
Franziska Ledig <fl32zyvy@studserv.uni-leipzig.de>
2024-10-12 14:01:56 +0200

    Bildbeschreibun

--- scripts/interaktiv.js
+++ scripts/interaktiv.js
@@ -1,7 +1,16 @@
-const button = document.querySelector("button");
+/*const button = document.querySelector("button");
 
 button.addEventListener("click", updateName);
 
 function updateName() {
   window.location.href "history.html";
-}
+
+}*/
+
+ function describePicture(){
+        alert("Das Bild zeigt das Siedlungsgebiet Donaumoos heute. Rote Punkte lassen dichte Besiedlung entlang von Straßen und einige kleinere Ballungsgebiete erkennen. ");
+
+
+ function actiont(){
+    alert('djjd');
+ }
